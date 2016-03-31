c8e74a76a2ef1877fa0013733b9507eff307ce50
Igor Nazarov <i.nazarov@samsung.com>
2016-03-31 13:23:24 +0300

    TizenRefApp-6041 [Email] Refactor SDK project structure for making CLI files in the root folder
    Change-Id: I70277bd2a26f010781a20fe6655b719b97ed6a31

--- SDK-3.0-Projects/email/copy-res.sh
+++
@@ -1,7 +0,0 @@
-rm -rf res/images
-rm -rf res/misc
-rm -rf res/po
-
-cp -r ../../Project-Files/resources/images res/images
-cp -r ../../Project-Files/resources/misc res/misc
-cp -r ../../Project-Files/resources/po res/po
