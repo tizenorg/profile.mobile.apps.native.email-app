555279a6f5fd950d9135716e4f2469867757508e
Andriy Kolesnichenko <a.kolesniche@samsung.com>
2016-06-10 10:46:46 +0300

    TizenRefApp-6435 [Email] Implement "Forward to all" and Include/exclude attachments from Viewer to Composer
    Change-Id: Ifc7e5feb6abbc5b2e32735430dad9ab4f1d72546
    Signed-off-by: Andriy Kolesnichenko <a.kolesniche@samsung.com>

--- modules/composer/src/email-composer-send-mail.c
+++ modules/composer/src/email-composer-send-mail.c
@@ -418,7 +418,7 @@ static COMPOSER_ERROR_TYPE_E _send_mail(EmailComposerView *view)
 					&view->org_mail_info->mail_data->mail_id, 1, EMAIL_MAIL_ATTRIBUTE_REPLIED_TIME, sending_time);
 			email_engine_set_flags_field(view->account_info->original_account->account_id,
 					&view->original_mail_id, 1, EMAIL_FLAGS_ANSWERED_FIELD, 1, 1);
-		} else if (view->composer_type == RUN_COMPOSER_FORWARD) {
+		} else if (view->composer_type == RUN_COMPOSER_FORWARD || view->composer_type == RUN_COMPOSER_FORWARD_ALL) {
 			email_engine_update_mail_attribute(view->org_mail_info->mail_data->account_id,
 					&view->org_mail_info->mail_data->mail_id, 1, EMAIL_MAIL_ATTRIBUTE_FORWARDED_TIME, sending_time);
 			email_engine_set_flags_field(view->account_info->original_account->account_id,
